afe0376b9f912c055fdb389f7790766ae6d097cf
yyltesting <791482765@qq.com>
2019-06-20 18:12:58 +0800

    更新提交

---
+++ AutoTest/src/AutoApi/content/Login/Logincontent.java
@@ -0,0 +1,13 @@
+package AutoApi.content.Login;
+
+import AutoApi.content.URL.URLtestcontent;
+
+public class Logincontent {
+	public static String uri = URLtestcontent.url+"/getcookies";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AutoTest/src/AutoApi/content/URL/URLtestcontent.java
@@ -0,0 +1,13 @@
+package AutoApi.content.URL;
+
+public class URLtestcontent {
+	public static  String url = "http://localhost:8899";
+	
+			
+			
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
